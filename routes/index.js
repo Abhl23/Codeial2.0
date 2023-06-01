aa2c90a86406c0988b9284b3a1bd6097096e5112
Abhishek Bhatt <bhatt23abhishek@gmail.com>
2023-06-01 22:24:05 +0530

    set the view engine

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ console.log('Router is loaded!');
 
 router.get('/', homeController.home);
 router.use('/users', require('./users'));
+router.use('/posts', require('./posts'));
 
 // for any further routes, access from here
 // router.use('/routeName', require('./routerName')); 
