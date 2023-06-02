9ff281c1292dccf701757fbc9810f0ecf63c16a3
Abhishek Bhatt <bhatt23abhishek@gmail.com>
2023-06-03 01:26:47 +0530

    create sign up functionality

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,25 +1,45 @@
-module.exports.profile=function(req, res){
-    return res.render('user_profile', {
-        title: "User Profile"
-    });
+const User = require("../models/user");
+
+module.exports.profile = function (req, res) {
+  return res.render("user_profile", {
+    title: "User Profile",
+  });
 };
 
 // render the sign up page
-module.exports.signUp=function(req, res){
-    return res.render('user_sign_up', {
-        title: "Codeial | Sign Up"
-    });
+module.exports.signUp = function (req, res) {
+  return res.render("user_sign_up", {
+    title: "Codeial | Sign Up",
+  });
 };
 
 // render the sign in page
-module.exports.signIn=function(req, res){
-    return res.render('user_sign_in', {
-        title: "Codeial | Sign In"
-    });
+module.exports.signIn = function (req, res) {
+  return res.render("user_sign_in", {
+    title: "Codeial | Sign In",
+  });
 };
 
 // get the sign up data
-module.exports.create=function(req, res){};
+module.exports.create = async function (req, res) {
+  if (req.body.password != req.body.confirm_password)
+    return res.redirect("back");
+
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    if(!user){
+        await User.create(req.body);
+        return res.redirect('/users/sign-in');
+    }
+    else
+        return res.redirect('back');
+        
+  } catch (err) {
+    console.log(`Error: ${err}`);
+    return;
+  }
+};
 
 // sign-in and create a session for the user
-module.exports.createSession=function(req, res){};
+module.exports.createSession = function (req, res) {};
