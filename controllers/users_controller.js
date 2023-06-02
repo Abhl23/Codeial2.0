b556473ad237afaab31920f942e6f47af51774a6
Abhishek Bhatt <bhatt23abhishek@gmail.com>
2023-06-02 22:10:18 +0530

    add actions and forms for sign-in and sign-up page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -17,3 +17,9 @@ module.exports.signIn=function(req, res){
         title: "Codeial | Sign In"
     });
 };
+
+// get the sign up data
+module.exports.create=function(req, res){};
+
+// sign-in and create a session for the user
+module.exports.createSession=function(req, res){};
