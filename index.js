7b12ec4885b7737ae13592da9077638edfbb4d8d
Abhishek Bhatt <bhatt23abhishek@gmail.com>
2023-06-03 01:10:27 +0530

    setup body and cookie parser

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 const express = require("express");
-const expressLayouts=require('express-ejs-layouts');
-const db=require('./config/mongoose');
+const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const db = require("./config/mongoose");
 
 const port = 8000;
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
 // use static files
-app.use(express.static('./assets'));
+app.use(express.static("./assets"));
 // extract styles and scripts from sub pages into the layout
-app.set('layout extractStyles', true);
-app.set('layout extractScripts', true);
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 // use express layouts
 app.use(expressLayouts);
 
 // use express router
-app.use('/', require('./routes'));
+app.use("/", require("./routes"));
 
 // set the view engine
-app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 app.listen(port, function (err) {
   if (err) {
