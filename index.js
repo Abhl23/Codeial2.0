59597de5bcddcd595839a799ff38014f4f4d0c28
Abhishek Bhatt <bhatt23abhishek@gmail.com>
2023-06-04 02:29:00 +0530

    use connect-mongo to store session cookie in db

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const cookieParser = require("cookie-parser");
 const db = require("./config/mongoose");
 
 // used for session cookie
-const session=require('express-session');
-const passport=require('passport');
-const passportLocal=require('./config/passport-local-strategy');
+const session = require("express-session");
+const passport = require("passport");
+const passportLocal = require("./config/passport-local-strategy");
+const MongoStore = require("connect-mongo");
 
 const port = 8000;
 
@@ -29,16 +30,22 @@ app.set("layout extractScripts", true);
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
-app.use(session({
-  name: 'codeial',
-  // change the secret before deployment in production mode
-  secret: 'blahsomething',
-  saveUninitialized: false,
-  resave: false,
-  cookie: {
-    maxAge: (1000 * 60 * 100)
-  }
-}));
+app.use(
+  session({
+    name: "codeial",
+    // change the secret before deployment in production mode
+    secret: "blahsomething",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 1000 * 60 * 100,
+    },
+    store: MongoStore.create({
+      mongoUrl: "mongodb://localhost/codeial_development",
+      autoRemove: "disabled",
+    }),
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
