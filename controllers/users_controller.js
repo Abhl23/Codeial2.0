329555d17c85e5fe83392a5fd9847457b711554f
Abhishek Bhatt <bhatt23abhishek@gmail.com>
2023-06-03 23:04:13 +0530

    use passport to authenticate and express-session to encrypt session cookie

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -42,4 +42,6 @@ module.exports.create = async function (req, res) {
 };
 
 // sign-in and create a session for the user
-module.exports.createSession = function (req, res) {};
+module.exports.createSession = function (req, res) {
+  return res.redirect('/');
+};
--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const db = require("./config/mongoose");
 
+// used for session cookie
+const session=require('express-session');
+const passport=require('passport');
+const passportLocal=require('./config/passport-local-strategy');
+
 const port = 8000;
 
 const app = express();
@@ -13,19 +18,33 @@ app.use(cookieParser());
 
 // use static files
 app.use(express.static("./assets"));
+
+// use express layouts
+app.use(expressLayouts);
 // extract styles and scripts from sub pages into the layout
 app.set("layout extractStyles", true);
 app.set("layout extractScripts", true);
 
-// use express layouts
-app.use(expressLayouts);
-
-// use express router
-app.use("/", require("./routes"));
-
 // set the view engine
 app.set("view engine", "ejs");
 app.set("views", "./views");
+
+app.use(session({
+  name: 'codeial',
+  // change the secret before deployment in production mode
+  secret: 'blahsomething',
+  saveUninitialized: false,
+  resave: false,
+  cookie: {
+    maxAge: (1000 * 60 * 100)
+  }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// use express router
+app.use("/", require("./routes"));
 
 app.listen(port, function (err) {
   if (err) {
