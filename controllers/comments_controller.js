b2325883458d71d2b037dbc9738632ca196e38b6
Abhishek Bhatt <bhatt23abhishek@gmail.com>
2023-06-06 20:55:11 +0530

    delete a comment with authorization checks

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -12,8 +12,12 @@ module.exports.create = async function (req, res) {
         post: req.body.post,
       });
 
-      post.comments.push(comment);
-      post.save();
+      await Post.findByIdAndUpdate(req.body.post, {
+        $push: { comments: comment.id },
+      });
+
+      // post.comments.push(comment);
+      // post.save();
 
       return res.redirect("/");
     }
@@ -22,6 +26,40 @@ module.exports.create = async function (req, res) {
     return res.redirect("/");
   } catch (err) {
     console.log("Error in creating a comment in the db");
+    return;
+  }
+};
+
+module.exports.destroy = async function (req, res) {
+  try {
+    const comment = await Comment.findById(req.params.id);
+
+    if (!comment) {
+      console.log("Comment does not exist in the db");
+      return res.redirect("back");
+    }
+
+    const postId = comment.post;
+
+    const post = await Post.findByIdAndUpdate(postId, {
+      $pull: {
+        comments: req.params.id,
+      },
+    }).populate("user");
+    // authorization check for user of the comment OR user of the post
+    // only either of these can delete the comment
+    if (comment.user == req.user.id || post.user.id == req.user.id) {
+      await Comment.findByIdAndDelete(req.params.id);
+
+      return res.redirect("back");
+    }
+    else {
+      console.log("You do not have authorization to delete the comment");
+      return res.redirect("back");
+    }
+
+  } catch (err) {
+    console.log(`Error in deleting a comment from the db, ${err}`);
     return;
   }
 };
