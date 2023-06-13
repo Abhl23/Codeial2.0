d8445589ec21a9cbd8d8a910d38a48cda244d78a
Abhishek Bhatt <bhatt23abhishek@gmail.com>
2023-06-13 19:59:53 +0530

    upload profile pic and store its path in user schema using multer

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,42 +1,58 @@
 const User = require("../models/user");
 
 module.exports.profile = async function (req, res) {
-  try{
-    const user=await User.findById(req.params.id);
+  try {
+    const user = await User.findById(req.params.id);
 
     return res.render("user_profile", {
       title: "User Profile",
-      profile_user: user
+      profile_user: user,
     });
-  }catch(err){
-
+  } catch (err) {
     req.flash("error", err);
-    return res.redirect('back');
+    return res.redirect("back");
   }
 };
 
-module.exports.update=async function(req, res){
-  try{
-    if(req.user.id == req.params.id){
-      await User.findByIdAndUpdate(req.params.id, req.body);
+module.exports.update = async function (req, res) {
+  try {
+    if (req.user.id == req.params.id) {
+      // await User.findByIdAndUpdate(req.params.id, req.body);
 
-      req.flash('success', 'Profile Updated Successfully!');
-      return res.redirect('back');
+      let user = await User.findById(req.params.id);
+
+      User.uploadedAvatar(req, res, function (err) {
+        if (err) {
+          console.log(`Multer Error: ${err}`);
+          return;
+        }
+
+        user.name = req.body.name;
+        user.email = req.body.email;
+
+        if (req.file) {
+          console.log(req.file);
+          user.avatar = User.avatarPath + "/" + req.file.filename;
+        }
+
+        user.save();
+        req.flash("success", "Profile Updated Successfully!");
+        return res.redirect("back");
+      });
+    } else {
+      req.flash("error", "You are not authorized to update this profile!");
+      return res.redirect("back");
     }
-    else{
-      req.flash('error', "You are not authorized to update this profile!");
-      return res.redirect('back');
-    }
-  }catch(err){
-    req.flash('error', err);
-    return res.redirect('back');
+  } catch (err) {
+    req.flash("error", err);
+    return res.redirect("back");
   }
 };
 
 // render the sign up page
 module.exports.signUp = function (req, res) {
-  if(req.isAuthenticated()){
-    return res.redirect('/users/profile');
+  if (req.isAuthenticated()) {
+    return res.redirect("/users/profile");
   }
 
   return res.render("user_sign_up", {
@@ -46,8 +62,8 @@ module.exports.signUp = function (req, res) {
 
 // render the sign in page
 module.exports.signIn = function (req, res) {
-  if(req.isAuthenticated()){
-    return res.redirect('/users/profile');
+  if (req.isAuthenticated()) {
+    return res.redirect("/users/profile");
   }
 
   return res.render("user_sign_in", {
@@ -57,48 +73,44 @@ module.exports.signIn = function (req, res) {
 
 // get the sign up data
 module.exports.create = async function (req, res) {
-  if (req.body.password != req.body.confirm_password){
-
-    req.flash('error', "Password does not match!");
+  if (req.body.password != req.body.confirm_password) {
+    req.flash("error", "Password does not match!");
     return res.redirect("back");
   }
-    
 
   try {
     const user = await User.findOne({ email: req.body.email });
 
-    if(!user){
-        await User.create(req.body);
+    if (!user) {
+      await User.create(req.body);
 
-        req.flash('success', "Account Created Successfully!");
-        return res.redirect('/users/sign-in');
+      req.flash("success", "Account Created Successfully!");
+      return res.redirect("/users/sign-in");
+    } else {
+      req.flash("error", "Email already exists!");
+      return res.redirect("back");
     }
-    else{
-      req.flash('error', 'Email already exists!');
-      return res.redirect('back');
-    }
-        
   } catch (err) {
-    req.flash('error', err);
-    return res.redirect('back');
+    req.flash("error", err);
+    return res.redirect("back");
   }
 };
 
 // sign-in and create a session for the user
 module.exports.createSession = function (req, res) {
-  req.flash('success', 'Logged In Successfully!');
-  return res.redirect('/');
+  req.flash("success", "Logged In Successfully!");
+  return res.redirect("/");
 };
 
 // sign-out and delete the user's session
-module.exports.destroySession=function(req, res){
-  req.logout(function(err){
-    if(err){
-      req.flash('error', err);
-      return res.redirect('back');
+module.exports.destroySession = function (req, res) {
+  req.logout(function (err) {
+    if (err) {
+      req.flash("error", err);
+      return res.redirect("back");
     }
 
-    req.flash('success', 'You have logged out!');
-    return res.redirect('/');
+    req.flash("success", "You have logged out!");
+    return res.redirect("/");
   });
 };
--- models/user.js
+++ models/user.js
@@ -1,5 +1,10 @@
 const mongoose = require("mongoose");
 
+const multer = require("multer");
+const path = require("path");
+
+const AVATAR_PATH = path.join("/uploads/users/avatars");
+
 const userSchema = new mongoose.Schema(
   {
     email: {
@@ -15,11 +20,28 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    avatar: {
+      type: String,
+    },
   },
   {
     timestamps: true,
   }
 );
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "..", AVATAR_PATH)); // the destination to store the file in directory thats why absolute path needed
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix); // file.fieldname = name attribute of input type="file"
+  },
+});
+
+// static methods. Can be accessed through User model.
+userSchema.statics.uploadedAvatar = multer({ storage: storage }).single("avatar");            // used to upload a single file with the key "avatar" from the form data
+userSchema.statics.avatarPath = AVATAR_PATH;
 
 const User = mongoose.model("User", userSchema);
 
