d3dd889939e7c68fc97712f166e9733a1afa9ee2
Abhishek Bhatt <bhatt23abhishek@gmail.com>
2023-06-20 16:45:56 +0530

    send html template email for new comment

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -1,21 +1,28 @@
-const nodeMailer=require('../config/nodemailer');
+const nodeMailer = require("../config/nodemailer");
 
 // this is another way of exporting a method
-module.exports.newComment=(comment) => {
-    console.log('Inside newComment mailer', comment);
+module.exports.newComment = (comment) => {
+  let htmlString = nodeMailer.renderTemplate(
+    { comment: comment },
+    "/comments/new_comment.ejs"
+  );
 
-    nodeMailer.transporter.sendMail({
-        from: "codeial.updates@gmail.com",
-        to: comment.user.email,
-        subject: "New Comment Published!",
-        html: "<h1>Yup, your comment is now published!</h1>"
-    }, (err, info) => {                // info carries the information about the request that has been sent to gmail SMTP mailing server
-        if(err){
-            console.log(`Error in sending mail: ${err}`);
-            return;
-        }
-
-        console.log('Mail Delivered!', info);
+  nodeMailer.transporter.sendMail(
+    {
+      from: "codeial.updates@gmail.com",
+      to: comment.user.email,
+      subject: "New Comment Published!",
+      html: htmlString,
+    },
+    (err, info) => {
+      // info carries the information about the request that has been sent to gmail SMTP mailing server
+      if (err) {
+        console.log(`Error in sending mail: ${err}`);
         return;
-    });
+      }
+
+      console.log("Mail Delivered!", info);
+      return;
+    }
+  );
 };
