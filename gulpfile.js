dd88c03f875686f46c51c07c858e77de2854d6a0
Abhishek Bhatt <bhatt23abhishek@gmail.com>
2023-06-27 19:03:33 +0530

    create gulp task for building js, images and deleting the old build

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,12 @@ const sass = require("gulp-sass")(require("sass"));
 const cssnano = require("gulp-cssnano");
 const rev = require("gulp-rev");
 
+const uglify = require("gulp-uglify-es").default;
+
+const imagemin = require("gulp-imagemin");
+
+const del = require("del");
+
 gulp.task("css", function (done) {
   console.log("minifying css...");
 
@@ -29,3 +35,63 @@ gulp.task("css", function (done) {
 
   done();
 });
+
+gulp.task("js", function (done) {
+  console.log("minifying js...");
+
+  // compressing and renaming js files
+  gulp
+    .src("./assets/js/**/*.js")
+    .pipe(uglify())
+    .pipe(rev())
+    .pipe(gulp.dest("./public/assets/js"))
+    .pipe(
+      rev.manifest({
+        cwd: "public",
+        merge: true,
+      })
+    )
+    .pipe(gulp.dest("./public/assets"));
+
+  done();
+});
+
+gulp.task("images", function (done) {
+  console.log("compressing images...");
+
+  // compressing and renaming images
+  gulp
+    .src("./assets/images/**/*+(png|jpg|gif|svg|jpeg)") // regex(regular expression)
+    .pipe(imagemin())
+    .pipe(rev())
+    .pipe(gulp.dest("./public/assets/images"))
+    .pipe(
+      rev.manifest({
+        cwd: "public",
+        merge: true,
+      })
+    )
+    .pipe(gulp.dest("./public/assets"));
+
+  done();
+});
+
+// empty the old build from the public/assets directory brfore creating a new one
+gulp.task("clean:assets", function (done) {
+  console.log("cleaning previous build...");
+
+  del.sync(["./public/assets"], { force: true });
+
+  done();
+});
+
+// build task to run all 4 tasks
+gulp.task(
+  "build",
+  gulp.series("clean:assets", "css", "js", "images"),
+  function (done) {
+    console.log("Building assets...");
+
+    done();
+  }
+);
